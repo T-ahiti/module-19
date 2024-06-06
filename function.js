5ae63a412b9f4d0c82f053eb2590193114ba05ca
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-06 14:02:49 +0600

    second commit

--- function.js
+++ function.js
@@ -6,5 +6,14 @@ function startFan(){
     console.log('press switch');
 }
 
-// call the function
+// call the function, jotobar startFan(); eta likhbo totobar result dibe
+// startFan();
+// startFan();
+// startFan();
+
+startFan();
+console.log('waking up in the morning');
+startFan();
+console.log('eating lunch');
+console.log('watching js tutorial');
 startFan();
