f73a5c7baff9c411644d13e41648eeadd3952daa
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-06 13:58:01 +0600

    function added

---
+++ function.js
@@ -0,0 +1,10 @@
+
+// function declaration
+function startFan(){
+    console.log('stand up');
+    console.log('walk towards the switch');
+    console.log('press switch');
+}
+
+// call the function
+startFan();
