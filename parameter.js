f694fe413edc8a9fd9cdb78577ff442bafd053d7
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-07 20:53:43 +0600

    third commit

--- parameter.js
+++ parameter.js
@@ -1,8 +1,25 @@
 
 
-function bringSingara(money){
-    console.log('mama singara den');
-    console.log(money);
+// function bringSingara(money){
+    // console.log('mama singara den');
+    // function ta ke call kore jei parameter dibo otar man tai add hobe
+    // console.log('eto tk disen:', money);
+// }
+
+// bringSingara(100);
+
+
+// more parameter 
+// function add(num1, num2){
+    // console.log('going to add:', num1, num2);
+// }
+
+// add(125, 96);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
 }
 
-bringSingara(100);
+sum(15, 98, 56, 30, 54);
