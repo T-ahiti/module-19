2f63b2c1584ea902ae26663d0e02de5957861506
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-09 23:29:08 +0600

    fourth commit

--- return.js
+++ return.js
@@ -1,6 +1,6 @@
 
 function add(num1, num2){
-    console.log(num1, num2);
+    // console.log(num1, num2);
     var sum = num1 + num2;
     // console.log(sum);
     return sum;
@@ -8,6 +8,15 @@ function add(num1, num2){
 
 add(45, 15);
 var total = add(80, 20);
-    console.log('total', total);
+    // console.log('total', total);
 
 
+function bringShingara(money){
+    var singaraPrice = 10;
+    var quantity = money / singaraPrice;
+    return quantity;
+}
+
+bringShingara(200);
+var singaras = bringShingara(200);
+console.log(singaras);
