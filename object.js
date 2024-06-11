42f31b4f3809dd7bff82761b19494ec9fad04be6
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-11 17:34:40 +0600

    third commit

--- object.js
+++ object.js
@@ -11,3 +11,19 @@ var mobile = {
     storage: '64g',
     camera: '7MP'
 }
+
+var computer = {
+    brand: 'lenovo',
+    color: 'silver',
+    processor: 'i7',
+    price: 39000
+}
+
+
+console.log(computer);
+// . diye jei property likhbo just otar value show korbe tokhon
+console.log(computer.price);
+// value r man change kora jay .property diye
+computer.processor = 'i79';
+console.log(computer);
+
