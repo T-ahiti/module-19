a0d2f8eae1da3152b6f63ffd9ba4dbdeef381eb9
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-11 16:44:28 +0600

    elements added

---
+++ object.js
@@ -0,0 +1,13 @@
+var student = { 
+    id:115, 
+    name:'solayman khan', 
+    class:9, 
+    marks:90
+}
+
+var mobile = {
+    brand: 'samsung',
+    price: 10000,
+    storage: '64g',
+    camera: '7MP'
+}
