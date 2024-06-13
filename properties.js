c11189530f194c05f4e5b8ce0215cbc8e441f46f
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-14 00:32:28 +0600

    third commit

--- properties.js
+++ properties.js
@@ -7,5 +7,23 @@ var shoppingCart = {
     keyboard: 1
 }
 
+// when u know the specific property name.. use dot notation to get the property value
 var penCount = shoppingCart.pen;
-console.log(shoppingCart)
+// console.log(shoppingCart)
+
+// alternative system 1/another systm
+var penCount2 = shoppingCart['pen']
+
+// alternative system 2
+var propertyName = 'mouse';
+var propertyValue = shoppingCart[propertyName]
+console.log(propertyValue)
+
+
+// shobgula properties/keys er name dibe
+var properties = Object.keys(shoppingCart)
+// console.log(properties);
+
+// shobgula value ke dekhabe
+var propertyValues = Object.values(shoppingCart);
+// console.log(propertyValues);
