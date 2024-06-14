f44281919d228f978c5f63373d41e76e2260faee
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-14 14:56:11 +0600

    switch added

---
+++ switch.js
@@ -0,0 +1,19 @@
+const color = 'yellow';
+if (color === 'green'){
+    console.log(' you are my green frnd');
+}
+else if (color ==='blue'){
+    console.log('you r my blue frnd');
+}
+else if (color ==='red'){
+    console.log('you r my red frnd');
+}
+else if (color ==='yellow'){
+    console.log('you r my yellow frnd');
+}
+else if (color ==='white'){
+    console.log('you r my white frnd');
+}
+else{
+    console.log('you r my black frnd');
+}
